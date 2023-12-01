74db18fc6f2aa7360d982a38c3fac5ed56433835
Wanda Singela (Gubhela) <150936870+wandadrizzle@users.noreply.github.com>
2023-12-01 15:58:41 +0200

    ----

---
+++ AllLoops/Program.cs
@@ -0,0 +1,56 @@
+﻿string? readResult;
+bool validEntry = false;
+// capture user input in a string variable named readResult
+Console.Clear();
+Console.WriteLine("Enter a string containing at least three characters:");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
+
+Console.WriteLine();
+Console.WriteLine();
+
+string readNumberResult;
+int numericValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do{
+    readNumberResult = Console.ReadLine();
+    validNumber = int.TryParse(readNumberResult, out numericValue);
+
+    if(readNumberResult != null){
+
+        if(validNumber)
+        {
+            if(numericValue > 5 && numericValue < 10)
+            {
+            Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+            validNumber = true;
+            }
+            else
+            {
+            Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+            validNumber = false;
+            }
+        }
+        else
+        {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        validNumber = false;
+        }
+    }
+}while(validNumber == false);
---
+++ DoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+Random otherRandom = new();
+int current = 0;
+int current01 = otherRandom.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+    if (current >= 8) continue; //contintue to the next iteration
+    Console.WriteLine(current);
+} while (current != 7);
+
+Console.WriteLine();
+Console.WriteLine();
+while (current01 >= 3)
+{
+    Console.WriteLine(current01);
+    current01 = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current01}");
+
+//Notes:
+/*
+As you saw in this last step, the continue statement transfers execution to the end of the current iteration. 
+This behavior is different than the behavior we saw with the break statement. 
+The break statement terminates the iteration (or switch) and transfers control to the statement that follows the terminated statement. 
+If there is no statement after the terminated statement, then control transfers to the end of the file or method.
+*/
